f3cc16c1128acc6ef6178761ed30d8bea03f1dac
kostasbak <kostasbakalis@gmail.com>
2023-10-12 21:08:54 +0300

    component input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PersonComponent } from './person/person.component';
+import { Person } from './interfaces/person';
 
 @Component({
   selector: 'app-root',
@@ -13,5 +14,19 @@ export class AppComponent {
   name: string = "Kostas";
   lastName = "Bakalis";
 
-  
+    person: Person = {
+      givenName: 'Kostas',
+      surName: 'Bakalis',
+      age: 26,
+      email: 'kostasbakalis@gmail.com',
+      address: "Sarafi 1"
+    }
+
+    person2: Person = {
+      givenName: 'Dimitris',
+      surName: 'Lypiridis',
+      age: 24,
+      email: 'dimitrislyp@gmail.com',
+      address: "Vazelonos 9"
+    }
 }
--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Person } from '../interfaces/person';
 
 @Component({
   selector: 'app-person',
@@ -9,10 +10,12 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./person.component.css']
 })
 export class PersonComponent {
-  person = {
-    givenName: 'Kostas',
-    surName: 'Bakalis',
-    age: 26,
-    email: 'kostasbakalis@gmail.com'
+
+  @Input() person: Person = {
+    givenName: "The person's First Name",
+    surName: "The person's Last Name",
+    age: 0,
+    email: "The person's email",
+    address: "The person's address"
   }
 }
