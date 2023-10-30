56804ba2368d82c38430682882c2a63cb1f3df87
kostasbak <kostasbakalis@gmail.com>
2023-10-30 07:43:35 +0200

    user search and are you sure components

--- src/app/crud-demo/crud/delete-user/delete-user.component.ts
+++ src/app/crud-demo/crud/delete-user/delete-user.component.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { Person } from 'src/app/interfaces/person';
 import { AppService } from 'src/app/app.service';
 import { HttpClient } from '@angular/common/http';
+import { CrudUserSearchComponent } from '../../utils/crud-user-search/crud-user-search.component';
+import { PersonCardComponent } from 'src/app/person-card/person-card.component';
+import { DangerAreYouSureComponent } from '../../utils/danger-are-you-sure/danger-are-you-sure.component';
 
 @Component({
   selector: 'app-delete-user',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CrudUserSearchComponent, PersonCardComponent, DangerAreYouSureComponent],
   templateUrl: './delete-user.component.html',
   styleUrls: ['./delete-user.component.css']
 })
@@ -39,4 +42,26 @@ export class DeleteUserComponent {
   //       complete: () => {'Delete operation completed'}
   //   })
   // }
+
+  onUserFound(user: Person | undefined) {
+    if (user) {
+      this.foundUser = user;
+    }
+  }
+  onConfirm(iAmSure: boolean) {
+    if (iAmSure && this.foundUser) {
+      const id = this.foundUser.id ?? -1;
+
+      this.appService.deleteUser(id).subscribe({
+      next: (user) => {console.log(user); this.userDeleted.emit()},
+      error: (error) => {
+        console.log(error);
+        this.userNotFound = true;
+      },
+      complete: () => {'Delete operation completed'}
+    })
+    } else {
+      this.foundUser = undefined;
+    }
+  }
 }
---
+++ src/app/crud-demo/utils/crud-user-search/crud-user-search.component.ts
@@ -0,0 +1,39 @@
+import { Component, ElementRef, EventEmitter, Inject, Output, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Person } from 'src/app/interfaces/person';
+import { AppService } from 'src/app/app.service';
+
+@Component({
+  selector: 'app-crud-user-search',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './crud-user-search.component.html',
+  styleUrls: ['./crud-user-search.component.css']
+})
+export class CrudUserSearchComponent {
+  foundUser: Person | undefined
+  userNotFound = false
+  @ViewChild('userId') userIdInput!:ElementRef<HTMLInputElement>;
+  @Output() userFound = new EventEmitter<Person | undefined>
+
+  constructor(private appService: AppService = Inject(AppService)) {}
+
+  onSearch() {
+    const id = this.userIdInput.nativeElement.value;
+    this.appService.getUserById(parseInt(id)).subscribe({
+      next: (user) => {
+        this.foundUser = user;
+        this.userNotFound = false;
+        this.userFound.emit(this.foundUser)        
+      },
+      error: (error) => {
+        this.foundUser = undefined;
+        this.userNotFound = true;
+        console.log(error)
+      },
+      complete: () => {
+        console.log("Operation Completed")
+      }
+    })
+  }
+}
